bbb4bac113185948c31e875798fc189b85caef1f
ChenCMD <scnmed.g@gmail.com>
2025-07-06 07:32:48 +0900

    島: BossDatapack 対応

--- src/registers/createIslands.ts
+++ src/registers/createIslands.ts
@@ -5,7 +5,7 @@ import { readFile, writeFile } from "../utils/io";
 import { parseCsv } from "../utils/csv";
 import { mkRegisterCommand } from "./common";
 
-type SpreadsheetColumns = [id: string, name: string, difficulty: string, dimension: string, x: string, y: string, z: string, tpCmd: string, tpCmd2: string, pos: string, rotation: string, a: string, b: string, c: string, bossId: string];
+type SpreadsheetColumns = [id: string, name: string, difficulty: string, dimension: string, x: string, y: string, z: string, tpCmd: string, tpCmd2: string, pos: string, rotation: string, a: string, b: string, c: string, bossId: string, bossDatapack: string];
 interface IslandData {
   id: number,
   name: string,
@@ -13,6 +13,7 @@ interface IslandData {
   pos: Vector3D,
   rotation: string,
   bossId: string,
+  bossDatapack: string,
 }
 
 export async function genIslandRegistry(inputPath: string, outputPath: string) {
@@ -30,9 +31,10 @@ export async function genIslandRegistry(inputPath: string, outputPath: string) {
         pos: new Vector3D(pos[0], pos[1], pos[2]),
         rotation: data[10],
         bossId: data[14],
+        bossDatapack: data[15],
       };
     })
-    .forEach(({ id, pos, rotation, bossId }) => {
+    .forEach(({ id, pos, rotation, bossId, bossDatapack }) => {
       const content: string[] = [
         makeIMPDoc(
           `asset:island/${id}/register`,
@@ -46,6 +48,7 @@ export async function genIslandRegistry(inputPath: string, outputPath: string) {
         register.set("Pos ([int] @ 3)", "Pos", `[${pos.x}, ${pos.y}, ${pos.z}]`),
         register.set("Rotation (string)", "Rotation", `${rotation}f`),
         register.set("BossID (string)", "BossID", bossId, bossId === ""),
+        register.set("BossDatapack (string)", "BossDatapack", `"${bossDatapack}"`, bossDatapack === ""),
       ];
       writeFile(path.join(outputPath, `island/${id}/register.mcfunction`), content.join("\n"));
     });
